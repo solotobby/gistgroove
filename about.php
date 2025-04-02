19ead82f72832fdbda302366238f65448df87fd8
Oluwatobi Solomon <88986301+solotobby@users.noreply.github.com>
2025-04-02 20:07:46 +0100

    Update about.php

--- about.php
+++ about.php
@@ -182,7 +182,7 @@
 <body>
     <header>
         <nav>
-            <h1><a href="/" style="color: white; text-decoration: none;"><img src="gistlogo.png" height="150" width="250"> Gistgroove</a></h1>
+            <h1><a href="/" style="color: white; text-decoration: none;"><img src="gistlogo.png" height="150" width="250"></a></h1>
         </nav>
     </header>
 
@@ -203,7 +203,7 @@
                 </div>
                 <div>
                    
-                    <h1 style="margin: 1rem 0;"> Privacy Policy </h1>
+                    <h1 style="margin: 1rem 0;"> About Us </h1>
                     
                 </div>
             </div>
