0780bc29440e2ce8f214d4ddd712b066ee8dd1b3
Oluwatobi Solomon <88986301+solotobby@users.noreply.github.com>
2025-04-02 19:08:48 +0100

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Privacy Policy</title>
+    <title>Aout Us</title>
     <style>
         :root {
             --primary: #6c5ce7;
